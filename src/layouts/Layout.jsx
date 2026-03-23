7f7d99dd8eb854192cf3f52ad9bd3d4a9c6741eb
Tanishk Kamble <tanishkkamble1707@gmail.com>
2026-03-23 11:06:56 +0530

    Add persistent sidebar, new logo, and layout component

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar";
+import Sidebar from "../components/dashboard/Sidebar";
 
 export default function Layout() {
   return (
