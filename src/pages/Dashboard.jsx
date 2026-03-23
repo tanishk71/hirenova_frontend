9b28f3bc2ab99d7e9be2ca9a115278dc33d15cb0
Tanishk Kamble <tanishkkamble1707@gmail.com>
2026-03-23 11:17:57 +0530

    Add persistent sidebar, new logo, and layout component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,26 +1,17 @@
 import "../styles/dashboard.css";
-import Sidebar from "../components/dashboard/Sidebar";
 import TopHeader from "../components/dashboard/TopHeader";
 import Kpis from "../components/dashboard/Kpis";
 import RecommendedJobs from "../components/dashboard/RecommendedJobs";
 import BottomSection from "../components/dashboard/BottomSection";
 
-
 export default function Dashboard() {
   return (
-    <div className="min-h-screen gradient-bg text-white overflow-hidden">
-      <div className="flex h-screen">
-        <Sidebar />
-
-        <main className="flex-1 overflow-auto p-8">
-          <TopHeader />
-          <Kpis />
-          <RecommendedJobs />
-          <BottomSection />
-          
-          <div className="h-6" />
-        </main>
-      </div>
+    <div className="text-white">
+      <TopHeader />
+      <Kpis />
+      <RecommendedJobs />
+      <BottomSection />
+      <div className="h-6" />
     </div>
   );
 }
